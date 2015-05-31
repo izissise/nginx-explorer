ee91c0169fdb4781d188847700d2101b1e4e9365
Hugues Morisset <morisset.hugues@gmail.com>
2015-05-31 22:34:23 +0200

    Correctly working for simple files

--- src/js/files.js
+++ src/js/files.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   loadJSON(config.dataset.api,
     function(data) {
-      formatData(data);
+      formatData(config.dataset.api, data);
 
       var dataTable = ConvertJsonToTable(data, 'dataTable', null, null);
       config.innerHTML = dataTable;
@@ -15,13 +15,12 @@ window.onload = function() {
 Number.prototype.padLeft = function(base,chr){
     var  len = (String(base || 10).length - String(this).length)+1;
     return len > 0? new Array(len).join(chr || '0')+this : this;
-}
+};
 
-function formatData(data) {
-
+function formatData(baseUrl, data) {
   //Transform as a link or directory
   data.forEach(function(e) {
-   var name = e.name;
+   var name = linkify(baseUrl, e.name);
    var d = new Date(e.mtime);
    d = [d.getDate().padLeft(),
                (d.getMonth()+1).padLeft(),
@@ -29,11 +28,10 @@ function formatData(data) {
               [d.getHours().padLeft(),
                d.getMinutes().padLeft(),
                d.getSeconds().padLeft()].join(':');
-               var size = ""
-               if (e.size != null) {
-                 size = e.size
-               }
-
+   var size = "";
+   if (e.size) {
+      size = humanFileSize(e.size, false);
+   }
 
    e.Filename = name;
    e.Size = size;
@@ -45,4 +43,23 @@ function formatData(data) {
    delete e.mtime;
   });
 
+}
+
+function linkify(base, data) {
+   return '<a href="' + base + data + '">' + data + '</a>';
+}
+
+function humanFileSize(bytes, si) {
+    var thresh = si ? 1000 : 1024;
+    if(Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+    var units = si ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+        : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+    var u = -1;
+    do {
+        bytes /= thresh;
+        ++u;
+    } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+    return bytes.toFixed(1)+' '+units[u];
 }
