50005fd5d3fb95c0bf3c39636aa5e48c60f8619f
Hugues Morisset <morisset.hugues@gmail.com>
2015-05-31 22:35:55 +0200

    Begin to format data

--- src/js/files.js
+++ src/js/files.js
@@ -3,9 +3,46 @@ window.onload = function() {
 
   loadJSON(config.dataset.api,
     function(data) {
-      var dataTable = ConvertJsonToTable(data, 'dataTable', null, 'Download');
+      formatData(data);
+
+      var dataTable = ConvertJsonToTable(data, 'dataTable', null, null);
       config.innerHTML = dataTable;
       new Tablesort(document.getElementById("dataTable"));
     },
     function(err) { console.error(err); });
 };
+
+Number.prototype.padLeft = function(base,chr){
+    var  len = (String(base || 10).length - String(this).length)+1;
+    return len > 0? new Array(len).join(chr || '0')+this : this;
+}
+
+function formatData(data) {
+
+  //Transform as a link or directory
+  data.forEach(function(e) {
+   var name = e.name;
+   var d = new Date(e.mtime);
+   d = [d.getDate().padLeft(),
+               (d.getMonth()+1).padLeft(),
+               d.getFullYear()].join('/') +' ' +
+              [d.getHours().padLeft(),
+               d.getMinutes().padLeft(),
+               d.getSeconds().padLeft()].join(':');
+               var size = ""
+               if (e.size != null) {
+                 size = e.size
+               }
+
+
+   e.Filename = name;
+   e.Size = size;
+   e.Date = d;
+
+   delete e.size;
+   delete e.name;
+   delete e.type;
+   delete e.mtime;
+  });
+
+}
